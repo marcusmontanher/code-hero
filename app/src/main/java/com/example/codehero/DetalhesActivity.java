d230736cb719cb311d50812b71bebf0338890658
Marcus Montanher <marcusmontanher@hotmail.com>
2021-03-28 19:19:10 -0300

    refactor: exibição de carregando na tela de detalhes

--- app/src/main/java/com/example/codehero/DetalhesActivity.java
+++ app/src/main/java/com/example/codehero/DetalhesActivity.java
@@ -1,13 +1,19 @@
 package com.example.codehero;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.codehero.databinding.ActivityDetalhesBinding;
 import com.example.codehero.model.character.Result;
 import com.google.gson.Gson;
@@ -34,8 +40,22 @@ public class DetalhesActivity extends AppCompatActivity {
         binding.setPersonagemNome(personagemNome);
         binding.setPersonagemDescricao(personagemDescricao);
 
+        binding.progressImg.setVisibility(View.VISIBLE);
 
-        Glide.with(this).load(personagemImagem).error(R.drawable.ic_no_image).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).into(binding.imgCharacter);
+        Glide.with(this).load(personagemImagem).listener(new RequestListener<Drawable>(){
+
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                binding.progressImg.setVisibility(View.GONE);
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                binding.progressImg.setVisibility(View.GONE);
+                return false;
+            }
+        }).error(R.drawable.ic_no_image).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).into(binding.imgCharacter);
 
         setSupportActionBar(binding.toolbarDetalhes);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
